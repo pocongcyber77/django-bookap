97d6baf5a1aa4ce397f6b92924a899520f56532e
Pocongcyber77 <nathaillah29@gmail.com>
2025-06-16 22:16:22 +0700

    Refine start command and static file collection for Railway deployment

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Set PYTHONPATH to include the project directory
+export PYTHONPATH=$PYTHONPATH:/app
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run Gunicorn
 gunicorn library_project.wsgi:application
